cedd6e9a9e0e532c42469c76f1de87b72a0cd1ff
Özge <ozgetuncerr@hotmail.com>
2023-11-10 14:55:56 +0300

    Create user-login-detection.py

---
+++ user-login-detection.py
@@ -0,0 +1,11 @@
+username = 'ozgetuncer'
+password = '1234'
+
+
+if (username == 'ozgetuncer'):
+    if (password == '1234'):
+        print('Hoşgeldiniz')
+    else:
+        print('Parolayı yanlış girdiniz. Tekrar deneyiniz.')
+else:
+    print('Username yanlış girdiniz. Tekrar Deneyiniz.')
